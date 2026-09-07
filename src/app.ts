385171e047e69f252d4502ab95ea096504338408
JefersonGomez <gomesjeje504@gmail.com>
2026-09-06 22:34:32 -0600

    documentacion con swagger

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import { router as categoryRouter } from "./modules/categories/category.routes.t
 import { router as productRouter } from "./modules/products/products.routes.ts"
 import { router as reportRouter } from "./modules/reports/report.routes.ts"
 import { router as movementRouter } from "./modules/movements/mevemenst.routes.ts"
+import fs from "fs"
+import {parse} from "yaml"
+import swaggerUi from "swagger-ui-express"
+import path from 'node:path';
 const app = Express()
 app.use(Express.json())
 
@@ -17,6 +21,12 @@ app.get("/health",(req, res)=>{
     res.status(200).json({"status":"ok"})
 })
 
+const openapiPath = path.join(process.cwd(), 'openapi.yaml');
+const file = fs.readFileSync(openapiPath, 'utf8');
+const documentParse = parse(file);
+
+// ✅ Pasa swaggerUi.serve y swaggerUi.setup() juntos en app.use
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(documentParse));
 
 
 export default app;
